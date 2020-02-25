992fefd5ab2c7c67e8861ca2f144783e90df4587
Aelpecyem <aelpecyem@gmail.com>
2020-02-25 20:35:50 +0100

    add patchouli and curios

--- src/main/java/com/example/examplemod/Elementaristics.java
+++ src/main/java/com/example/examplemod/Elementaristics.java
@@ -19,13 +19,11 @@ import org.apache.logging.log4j.Logger;
 import java.util.stream.Collectors;
 
 // The value here should match an entry in the META-INF/mods.toml file
-@Mod("examplemod")
-public class ExampleMod
-{
-    // Directly reference a log4j logger.
-    private static final Logger LOGGER = LogManager.getLogger();
+@Mod("elementaristics")
+public class Elementaristics {
+    public static final Logger LOGGER = LogManager.getLogger();
 
-    public ExampleMod() {
+    public Elementaristics() {
         // Register the setup method for modloading
         FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
         // Register the enqueueIMC method for modloading
