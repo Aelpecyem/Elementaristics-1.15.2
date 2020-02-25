b42a0a2e43011162af5495e8098fa0e0b5c6d310
Aelpecyem <aelpecyem@gmail.com>
2020-02-25 22:36:24 +0100

    optimize imports

--- src/main/java/de/aelpecyem/elementaristics/reg/ModItems.java
+++ src/main/java/de/aelpecyem/elementaristics/reg/ModItems.java
@@ -2,22 +2,12 @@ package de.aelpecyem.elementaristics.reg;
 
 import de.aelpecyem.elementaristics.Elementaristics;
 import de.aelpecyem.elementaristics.lib.Constants;
-import de.aelpecyem.elementaristics.lib.Util;
-import net.minecraft.block.Block;
-import net.minecraft.block.SlabBlock;
-import net.minecraft.block.StairsBlock;
-import net.minecraft.block.material.Material;
-import net.minecraft.block.material.MaterialColor;
-import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
-import net.minecraftforge.common.ToolType;
 import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.registries.IForgeRegistry;
 import net.minecraftforge.registries.ObjectHolder;
-
-import static de.aelpecyem.elementaristics.lib.Constants.BlockNames;
 
 @ObjectHolder(Constants.MOD_ID)
 @Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.MOD)
